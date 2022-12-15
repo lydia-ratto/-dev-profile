1317f2bdf176c6fcd0d44bb87d706074d826fffb
Lydia Ratto <lydiaratto@gmail.com>
2022-12-15 16:53:56 +0000

    feat: crearted main structure of the page and started project banner feature

---
+++ app/javascript/controllers/projects_reveal_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["banner"]
+  connect() {
+    console.log("The 'projects reveal' controller is now loaded!")
+  }
+
+  reveal() {
+    console.log("reveal");
+    this.bannerTarget.classList.remove("hidden");
+  }
+}
