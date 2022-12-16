e9bcb521f4ff80bee08fcde56bc48b394571d290
Lydia Ratto <lydiaratto@gmail.com>
2022-12-16 15:29:18 +0000

    added stimulus reveal

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,15 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+import { Application } from "@hotwired/stimulus"
+import ScrollTo from 'stimulus-scroll-to'
+
+const application = Application.start()
+application.register('scroll-to', ScrollTo)
+
+// Configure Stimulus development experience
+application.debug = false
+window.Stimulus   = application
+
+export { application }
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,8 +1,10 @@
 import { Application } from "@hotwired/stimulus"
+import ScrollTo from 'stimulus-scroll-to'
 
 const application = Application.start()
 
 // Configure Stimulus development experience
+application.register('scroll-to', ScrollTo)
 application.debug = false
 window.Stimulus   = application
 
