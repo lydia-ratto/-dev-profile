fd3a547b7da58d992f0f9db3270527910eba1d2a
Lydia Ratto <lydiaratto@gmail.com>
2022-12-30 22:46:03 +0000

    fix: added import application to line scroll controller, changed reveal function so it targeted the project-text class

--- app/javascript/controllers/projects_reveal_controller.js
+++ app/javascript/controllers/projects_reveal_controller.js
@@ -3,10 +3,10 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["banner", "image"]
   connect() {
-    console.log("The 'projects reveal' controller is now loaded!")
+    console.log("The 'projects!!!!' controller is now loaded!")
 
     const reveal = (event) => {
-      event.currentTarget.children[3].classList.toggle("hidden")
+      event.currentTarget.querySelector('.project-text').classList.toggle("hidden");
       // event.currentTarget.children[1].classList.toggle("active")
 
     }
